e56c00f976b7f7006078cac0eb5801fd13fc8755
logicHacker <1436266185@qq.com>
2017-08-07 21:09:27 +0800

    completed

--- NumPy_demo/3 ArrayCalculate.py
+++ NumPy_demo/3 ArrayCalculate.py
@@ -9,15 +9,52 @@ print('''
 # 简单的标量运算
 ''')
 arr = np.array([[1., 2., 3.], [4., 5., 6.], [7., 8., 9.]], dtype=int)    # 创建二维数组
-print("元素乘arr*arr\n", arr*arr)   # 行列号相同的数组元素间运算
-print("元素减arr-arr\n", arr-arr)
-print("元素倒数1/arr\n", 1/arr)
+print("# 元素乘arr*arr\n", arr*arr)   # 行列号相同的数组元素间运算
+print("# 元素减arr-arr\n", arr-arr)
+print("# 元素倒数1/arr\n", 1/arr)
 print("常数乘法arr*0.5\n", arr*0.5)
 print("# 计算各元素的平方根sqrt(arr)\n也可以用（arr**0.5）\n", np.sqrt(arr))
 print("# 计算各元素指数exp(arr);  \nabs  #绝对值;\n", exp(arr))
-print("add(arr,arr) #两数组中对应元素相加;  \nsubtract # 相减;  \nmultiply # 相乘;  \ndivide # 相除;", np.add(arr, arr))
+print("add(arr,arr) #两数组中对应元素相加;  \nsubtract # 相减;  \nmultiply # 相乘;  \ndivide # 相除;\n", np.add(arr, arr))
+
+print("# 维度约归也是一种常用的矢量化方法")
+print('''
+# 通过数组上的一组数学函数对整个数组或某个轴向的数据进行统计计算。
+# sum、mean以及标准差std等聚合计算
+# 既可以当做数组的实例方法调用，也可以当做顶级NumPy函数使用：
+''')
+
+arr2 = np.eye(3)+np.ones((3, 3))
+arr2[2, 1] = 0
+print("##################################################################")
+print("求和和平均")
+print("mean()方法是默认求所有元素的平均值", arr2.mean())
+print("sum()方法默认求返回对应轴的和的list\n", arr2.sum())
+print("# axis参数是轴的标号，sum()计算该轴上的统计值（0为列，1为行）\n", arr2.sum(axis=1))
+print('# 也可以对mean()使用', arr2.mean(axis=1))
 
 print('''
+#################################################################
+# 还可以将条件逻辑表述为数组运算
+# bools = (arr>0) 即可构造一个布尔数表
+# bools.any()    #用于测试数组中是否存在一个或多个True  
+# bools.all()    #用于测试数组中所有值是否都是True  
+''')
+
+bools = (arr>0)
+print("打印arr>0\n", bools)
+print("any()?:",  bools.any())
+print("all()?:", bools.all())
+
+print("# Numpy.where函数是三元表达式x if condition else y的矢量化版本")
+xarr = np.array([1.1, 1.2, 1.3, 1.4, 1.5])    # 两个数值数组
+yarr = np.array([2.1, 2.2, 2.3, 2.4, 2.5])
+cond = np.array([True, False, True, True, False])    # 一个布尔数组
+result = np.where(cond, xarr, yarr)    # 三元表达式
+print("两个数组，一个布尔数组即可组成 np.where(cond, xarr, yarr)\n", result)
+
+print('''
+#################################################################
 # 然后是线性代数运算
 # diag    以一维数组的形式返回方阵的对角线元素
 # dot     矩阵乘法
@@ -38,7 +75,12 @@ print("注意diag已经不在lin里了diag(arr)\n", diag(arr))
 print("矩阵相乘 arr.dot(arr)\n", arr.dot(arr))
 print("求行列式 det(arr)\n", lin.det(arr))
 arr = np.random.randn(3, 3)
-print("# 求逆inv(arr)(行列式为0会报错)\n", lin.inv(arr))
+print("# 求逆 inv(arr)(行列式为0会报错)\n", lin.inv(arr))
 
-print("# 然后是matrix的情况（可以通过Mat(array)函数得到，是array的子集，具有其所有特性")
+print("#################################################")
+print("# 最后是matrix的情况（可以通过Mat(array)函数得到，是array的子集，具有其所有特性")
+print("# 但是有许多便利的特性\n详情请看：http://blog.csdn.net/vincentlipan/article/details/20717163")
 
+am = array([1.5,  2.5])
+print("# 最后是一个特别神奇的操作\n", am)
+print("# am[:, np.newaxis]\n", am[:, np.newaxis])
--- NumPy_demo/4 ArrayQuote.py
+++ NumPy_demo/4 ArrayQuote.py
@@ -2,9 +2,9 @@ import numpy as np
 from numpy import *
 
 arr = np.arange(10)
-print("最开始的数组\n", arr)
-print("和其他语言的数组访问法一样，arr[5] =", arr[5])    # 索引第6个元素
-print("# 可以用arr[5:8] = 12\n令第6到第9个元素等于12")
+print("# 最开始的数组\n", arr)
+print("# 和其他语言的数组访问法一样，arr[5] =", arr[5])    # 索引第6个元素
+print("# 可以用 arr[5:8] = 12 令第6到第9个元素等于12")
 
 print("# arr[5:8] 切片功能可以索引第6到第9个元素(不含9)作为数组\n", arr[5:8])
 print("# 数组切片是原始数据的视图，视图上的任何修改都会反映到原数组")
@@ -25,12 +25,42 @@ print("# 索引第1列 \n", arr2d[:, :1])
 print("# 使用负数索引将从尾部开始选取行\n", arr2d[:-2])
 print("# 如果要按某个维度逆序，可以直接在切片第三个参数(步长)填-1\n", arr2d[::-1, :])
 
+print("##################################################")
 print("# 用同一个阵组合分块母阵，注意第二个参数是一个tuple，表示x行y列\n", tile(arr, (6, 1)))
+arr2d = np.arange(15).reshape((3, 5))
+print("# 生成顺序数组，后整形为3行5列 \n# arr.reshape((3,5))\n# 注意传入的是tuple\n", arr2d)
+print("# 转置数组 arr.T\n", arr2d.T)
+print("# 应用：利用np.dot计算矩阵内积XTX\n", np.dot(arr2d.T, arr2d))
+arr2d = np.random.randn(6, 3)
+print("# randn函数生成一些正态分布的随机数组（6行3列）\n", arr2d)
 
+print("###################################################")
+print("# 直接sort()的话会导致原数组丢失")
+arr.sort()
+print("# 一维数组排序需要sort()\n", arr)
 
-arr = np.arange(15).reshape((3, 5))
-print("# 生成顺序数组，后整形为3行5列arr.reshape((3,5))\n# 注意传入的是tuple\n", arr)
-print("# 转置数组 arr.T\n", arr.T)
-print("# 应用：利用np.dot计算矩阵内积XTX\n", np.dot(arr.T, arr))
-arr = np.random.randn(6, 3)
-print("# randn函数生成一些正态分布的随机数组（6行3列）\n", arr)
+arr2dcopy = arr2d.copy()
+arr2d.sort(0)
+print("# 二维数组排序需要指定轴(0为行，１为列)(该轴数据会被拆散)\n", arr2d)
+print("# 也可以返回排序后的坐标(垂直的轴不变，另一个轴是坐标)，方便保持原数组并且获得排序后结果\n", arr2dcopy.argsort(0))
+print("# 有一个唯一排序，也不是在原数组上操作，会返回一个不重复的排序好的数组")
+arraytosort = array([3, 3, 3, 2, 2, 1, 1, 4, 4])
+print("# 对", arraytosort, "unique()排序\n", unique(arraytosort))
+
+print("########################################################")
+print("# 最后是用于数组的文件输入输出")
+
+print("# 可以通过save(路径名,数组名)来保存数组(.npy),load\n# 并通过np.load(文件名)读取磁盘上的数组")
+np.save('.\\someArray', arr)
+np.load('.\\someArray.npy')
+print("# 也可以用savez()将多个数组以保存在一个压缩文件中")
+np.savez('.\\array_archive.npz', a=arr, b=arr)
+a = np.arange(0, 12, 0.5).reshape(4, -1)
+
+print("# 要保存为txt的话有几点需要注意，savetxt()，loadtxt()和上面参数可以一样，但是缺省按照’%.18e’格式保存数据，以空格分隔")
+np.savetxt('.\\a.txt', a)
+print(np.loadtxt('.\\a.txt'))
+print("# fmt改为保存为整数，delimiter改分隔符")
+np.savetxt('.\\a.txt', a, fmt="%d", delimiter=",")
+print("# 读取的时候也要记得指定delimiter")
+print(np.loadtxt('.\\a.txt', delimiter=","))
