5ff371eaec05b97492b599c7ce7dde6cb489a463
logicHacker <1436266185@qq.com>
2017-08-07 19:37:40 +0800

    aaa

--- NumPy_demo/3 ArrayCalculate.py
+++ NumPy_demo/3 ArrayCalculate.py
@@ -1,13 +1,21 @@
 import numpy as np
 from numpy import *
 
+print('''
+#######################################
+# 用数组表达式代替循环的做法，通常称为矢量化 #
+#######################################
 # 首先是array的情况
-# 首先是简单的标量运算
-arr = np.array([[1., 2., 3.], [4., 5., 6.], [7., 8., 9.]])    # 创建二维数组
-print(arr*arr)   # 行列号相同的数组元素间运算
-print(arr-arr)
-print(1/arr)
-print(arr*0.5)
+# 简单的标量运算
+''')
+arr = np.array([[1., 2., 3.], [4., 5., 6.], [7., 8., 9.]], dtype=int)    # 创建二维数组
+print("元素乘arr*arr\n", arr*arr)   # 行列号相同的数组元素间运算
+print("元素减arr-arr\n", arr-arr)
+print("元素倒数1/arr\n", 1/arr)
+print("常数乘法arr*0.5\n", arr*0.5)
+print("# 计算各元素的平方根sqrt(arr)\n也可以用（arr**0.5）\n", np.sqrt(arr))
+print("# 计算各元素指数exp(arr);  \nabs  #绝对值;\n", exp(arr))
+print("add(arr,arr) #两数组中对应元素相加;  \nsubtract # 相减;  \nmultiply # 相乘;  \ndivide # 相除;", np.add(arr, arr))
 
 print('''
 # 然后是线性代数运算
@@ -26,10 +34,11 @@ print('''
 
 import numpy.linalg as lin
 
-print("注意diag已经不在lin里了\n",diag(arr))
-print("矩阵相乘\n", arr.dot(arr))
-print("求行列式",lin.det(arr))
-# print(lin.inv(arr)) 求逆(行列式为0会报错)
+print("注意diag已经不在lin里了diag(arr)\n", diag(arr))
+print("矩阵相乘 arr.dot(arr)\n", arr.dot(arr))
+print("求行列式 det(arr)\n", lin.det(arr))
+arr = np.random.randn(3, 3)
+print("# 求逆inv(arr)(行列式为0会报错)\n", lin.inv(arr))
 
-# 然后是matrix的情况（可以通过Mat(array)函数得到，是array的子集，具有其所有特性）
+print("# 然后是matrix的情况（可以通过Mat(array)函数得到，是array的子集，具有其所有特性")
 
--- NumPy_demo/4 ArrayQuote.py
+++ NumPy_demo/4 ArrayQuote.py
@@ -6,15 +6,18 @@ print("最开始的数组\n", arr)
 print("和其他语言的数组访问法一样，arr[5] =", arr[5])    # 索引第6个元素
 print("# 可以用arr[5:8] = 12\n令第6到第9个元素等于12")
 
-print("# 切片功能可以索引第6到第9个元素(不含9)作为数组\n", arr[5:8])
+print("# arr[5:8] 切片功能可以索引第6到第9个元素(不含9)作为数组\n", arr[5:8])
 print("# 数组切片是原始数据的视图，视图上的任何修改都会反映到原数组")
 arr_slice = arr[5:8]
-arr_slice[:] = 64    # 将数组切片的全部元素改为64
+arr_slice[:] = 64
+print("# 取arr[5:8]切片并将其全部元素改为64")
 print("# 看看现在的数组\n", arr)
 
-arr[5:8].copy()    # 得到数组切片的一份副本
+print("# 得到数组切片的一份副本,用copy()方法\narr[5:8].copy()\n")
+print(arr[5:8].copy())
 
-arr2d=np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+arr2d = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+print("创建一个二维数组\n", arr2d)
 print("# 索引二维数组第3行\n", arr2d[2])
 print("# 用列表表示位置\n", arr2d[0, 2], "\n# 对比原本的\n", arr2d[0][2]) #等价索引1行3列元素
 print("# 索引第1行和第2行（不含第3行）\n", arr2d[:2])
@@ -23,3 +26,11 @@ print("# 使用负数索引将从尾部开始选取行\n", arr2d[:-2])
 print("# 如果要按某个维度逆序，可以直接在切片第三个参数(步长)填-1\n", arr2d[::-1, :])
 
 print("# 用同一个阵组合分块母阵，注意第二个参数是一个tuple，表示x行y列\n", tile(arr, (6, 1)))
+
+
+arr = np.arange(15).reshape((3, 5))
+print("# 生成顺序数组，后整形为3行5列arr.reshape((3,5))\n# 注意传入的是tuple\n", arr)
+print("# 转置数组 arr.T\n", arr.T)
+print("# 应用：利用np.dot计算矩阵内积XTX\n", np.dot(arr.T, arr))
+arr = np.random.randn(6, 3)
+print("# randn函数生成一些正态分布的随机数组（6行3列）\n", arr)
