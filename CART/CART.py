469b8f8ee0014b2421f53e0917725e44a567f9c4
logicHacker <1436266185@qq.com>
2017-08-08 21:46:39 +0800

    CART_complete

---
+++ CART/CART.py
@@ -0,0 +1,183 @@
+from numpy import *
+import numpy as np
+
+
+def loadDataSet(fileName):
+    dataMat = []
+    with open(fileName) as fr:
+        for line in fr.readlines():
+            curLine = line.strip().split('\t')
+            # 斗胆将map变为tmd array
+            fltLine = array(curLine, float)
+            dataMat.append(fltLine)
+    return dataMat
+
+
+def binSplitDataSet(dataMat, feature, value):
+    mat0 = dataMat[nonzero(dataMat[:, feature] > value)[0], :]#[0]
+    # print(nonzero(dataMat[:, feature] <= value))
+    # print(nonzero(dataMat[:, feature] <= value)[0])
+    # print(dataMat[nonzero(dataMat[:, feature] <= value)[0], :])
+    # print(dataMat[nonzero(dataMat[:, feature] <= value)[0], :][0])
+    mat1 = dataMat[nonzero(dataMat[:, feature] <= value)[0], :]#[0]
+    # mat0 = []
+    # mat1 = []
+    # print((nonzero(dataSet[:, feature] > value))[0])
+    # for index in (nonzero(dataSet[:, feature] > value))[0]:
+    #     mat0.append(dataSet[index, :][0])
+    # for index1 in (nonzero(dataSet[:, feature] <= value))[0]:
+    #     mat1.append(dataSet[index1, :][0])
+    return mat0, mat1
+
+
+# 回归树的叶子节点
+def regLeaf(data):
+   return mean(data[:, -1])
+
+
+# 回归树的求解方式
+def regError(data):
+    # print(var(data[:, -1]))
+    # print(shape(data))
+    return var(data[:, -1])*shape(data)[0]
+
+
+def createTree(data, leafType=regLeaf, errType=regError, ops=(1, 4)):
+    feat, model = chooseBestSplit(data, leafType, errType, ops)
+    if feat is None:
+        return model
+    retTree = {}
+    retTree['spInd'] = feat
+    retTree['spVal'] = model
+    lSet, rSet = binSplitDataSet(data, feat, model)
+    retTree['left'] = createTree(lSet, leafType, errType, ops=ops)
+    retTree['right'] = createTree(rSet, leafType, errType, ops=ops)
+    return retTree
+
+
+def chooseBestSplit(data, leafType=regLeaf, errorType=regError, ops=(1, 4)):
+    tolS = ops[0]
+    tolN = ops[1]
+    # 如果数据集已经只有一种结果，返回叶子节点
+    # if len(set(data[:, -1].T.tolist()[0])) == 1:
+    if len(set(data[:, -1].T)) == 1:
+        return None, leafType(data)
+    m, n = shape(data)
+    S = errorType(data)
+    bestS = inf
+    bestIndex = 0
+    bestValue = 0
+    for featIndex in range(n-1):
+        for splitVal in set(data[:, featIndex]):
+            mat0, mat1 = binSplitDataSet(data, featIndex, splitVal)
+            if (shape(mat0)[0] < tolN) or (shape(mat1)[0] < tolN):
+                continue
+            newS = errorType(mat0) + errorType(mat1)
+            if newS < bestS:
+                bestIndex = featIndex
+                bestValue = splitVal
+                bestS = newS
+    # 至此，循环结束，找到了最佳的分割点和最佳的取值点
+    # 若成效不是很大，就进行剪枝
+    if (S - bestS) < tolS:
+        return None, leafType(data)
+    mat0, mat1 = binSplitDataSet(data, bestIndex, bestValue)
+    # 若切分出的数据集很小，就剪枝
+    if shape(mat0)[0] < tolN or shape(mat1)[0] < tolN:
+        return None, leafType(data)
+    # 否则正常输出
+    return bestIndex, bestValue
+
+
+# dataSet = loadDataSet("./ex2.txt")
+# print(dataSet)
+# a = binSplitDataSet(eye(4), 1, 0.5)
+#
+#
+# myDat = loadDataSet("ex2.txt")
+# # 改mat为array
+# myMat = array(myDat, float)
+# tree = createTree(myMat)
+# print(tree)
+
+
+def isTree(obj):
+    return type(obj).__name__ == 'dict'
+
+
+def getMean(tree):
+    if isTree(tree['left']):
+        tree['left'] = getMean(tree['left'])
+    if isTree(tree['right']):
+        tree['right'] = getMean(tree['right'])
+    return (tree['left']+tree['right'])/2.0
+
+
+def prune(tree, testData):
+    if shape(testData)[0] == 0:
+        print("塌陷！")
+        return getMean(tree)
+    lSet, rSet = binSplitDataSet(testData, tree['spInd'], tree['spVal'])
+    # 还有子节点就向下递归
+    # if isTree(tree['left']) or isTree(tree['right']):
+    if isTree(tree['left']):
+        tree['left'] = prune(tree['left'], lSet)
+    if isTree(tree['right']):
+        tree['right'] = prune(tree['right'], rSet)
+    # 左右都是子节点的时候，
+    if not isTree(tree['left']) and not isTree(tree['right']):
+        errorNoMerge = sum(power(lSet[:, -1]-tree['left'], 2)) + sum(power(rSet[:, -1]-tree['right'], 2))
+        treeMean = (tree['left']+tree['right'])/2.0
+        errorMerge = sum(power(testData[:, -1]-treeMean, 2))
+        if errorMerge < errorNoMerge:
+            print("merging")
+            return treeMean
+        else:
+            return tree
+    else:
+        return tree # miaomiaomiao?
+
+
+def linearSolve(data):
+    # m, n = data.shape
+    # X = mat(ones((m, n-1)))
+    # # Y = mat(ones((m, 1)))
+    # Y = mat(data[:, -1])
+    # X[:, 0:n] = data[:, 0:n-1]
+    # # for i in range(0,n):
+    # #     x[:, i+1] = data[:, i]
+    # xTx = X.T * X
+    # if np.linalg.det(xTx) == 0.0:
+    #     raise NameError("singular")
+    # ws = xTx.I*(X.T*Y)
+    # return ws, X, Y
+    m, n = shape(data)
+    X = mat(ones((m, n)))
+    X[:, 1:n] = data[:, 0:n-1]
+    Y = mat(data[:, -1]).T #and strip out Y
+    xTx = X.T*X
+    if linalg.det(xTx) == 0.0:
+        raise NameError('This matrix is singular, cannot do inverse,\n\
+        try increasing the second value of ops')
+    d = np.linalg.inv(xTx) * mat(X.T)
+    ws = d * Y
+    # ws = np.linalg.inv(Y)
+    return ws, X, Y
+
+
+def modelLeaf(data):
+    ws, X, Y = linearSolve(data)
+    return ws
+
+
+def modelError(data):
+    ws, X, Y = linearSolve(data)
+    yHat = X * ws
+    return sum(power(Y-yHat, 2))
+
+
+data = array(loadDataSet("exp2.txt"), float)
+tree = createTree(data, leafType=modelLeaf, errType=modelError, ops=(1, 10))
+print(tree)
+# prune(tree, array(loadDataSet("ex2test.txt")))
+# print(tree)
--- kNN/kNN.py
+++ kNN/kNN.py
@@ -1,89 +1,94 @@
 from numpy import *
 import operator
+import matplotlib.pyplot as plt
+
 
 def createDataSet():
-    group = array([[1.0,1.1],[1.0,1.0],[0,0],[0,0.1]])
-    labels = ['A','A','B','B']
-    return group,labels
+    group = array([[1.0, 1.1], [1.0, 1.0], [0, 0], [0, 0.1]])
+    labels = ['A', 'A', 'B', 'B']
+    return group, labels
 
-group,labels = createDataSet()
+group, labels = createDataSet()
 
-def classify(inX,dataSet,labels,k):
-    dataSetSize = dataSet.shape[0]
-    #将矩阵作为分块子阵，创建dataSetSize行，1列的矩阵
-    diffMat = tile(inX,(dataSetSize,1))-dataSet
+
+def classify(inX, Set, thelabels, num):
+    dataSetSize = Set.shape[0]
+    # 将矩阵作为分块子阵，创建dataSetSize行，1列的矩阵
+    diffMat = tile(inX,(dataSetSize,1)) - Set
     sqDiffMat = diffMat**2
-    #将所有列向量求和
+    # 将所有列向量求和
     sqDistances = sqDiffMat.sum(axis=1)
     distances = sqDistances**0.5
 
-    #利用argsort()方法得到排序后的下标
+    # 利用argsort()方法得到排序后的下标
     sortedDistIndicies = distances.argsort()
 
-    #对k个邻居进行投票
+    # 对k个邻居进行投票
     classCount={}
-    for i in range(k):
-        voteIlabel = labels[sortedDistIndicies[i]]
-        #这一步就可以直接利用map.get()中的默认值，创建不存在的key
+    for i in range(num):
+        voteIlabel = thelabels[sortedDistIndicies[i]]
+        # 这一步就可以直接利用map.get()中的默认值，创建不存在的key
         classCount[voteIlabel] = classCount.get(voteIlabel,0)+1
     sortedClassCount = sorted(classCount.items(),key = operator.itemgetter(1),reverse=True)
     return sortedClassCount[0][0]
 
-def file2matrix(filename,spilt):
-    with open(filename) as fr:#这里进行了一个替换,使得文件读写更加安全
+
+def file2matrix(filename, spilt):
+    with open(filename) as fr: # 这里进行了一个替换,使得文件读写更加安全
         arrayOlines=fr.readlines()
         LinesNumber = len(arrayOlines)
-        returnMat = zeros((LinesNumber,3)) #先初始化一个零数组
-        index = 0 #读取第index行的数据
-        classLabelVector=[] #类别记录
+        mainMat = zeros((LinesNumber, 3)) # 先初始化一个零数组
+        index = 0 # 读取第index行的数据
+        clasRecord=[] # 类别记录
         for line in arrayOlines:
             line = line.strip()
             listFromLine = line.split(spilt)
-            returnMat[index,:]=listFromLine[0:3] #对某一行进行读取
-            classLabelVector.append(int(listFromLine[-1])) #加入对应的结果
+            mainMat[index, :]= listFromLine[0:3] # 对某一行进行读取
+            clasRecord.append(int(listFromLine[-1])) # 加入对应的结果
             index += 1
-    return returnMat,classLabelVector
+    return mainMat, clasRecord
 
-returnMat,classLabelVector = file2matrix("datingTestSet2.txt","\t")
-print(returnMat)
-print(classLabelVector)
 
-import matplotlib
-import matplotlib.pyplot as plt
-def mat2gra(mat,result):
-    fig = plt.figure()  ##新建一个视图
+def mat2gra(mat, result):
+    fig = plt.figure()  # 新建一个视图
     ax = fig.add_subplot(111)  ##在视图的中央建立一个子图
-    ax.scatter(mat[:,1], mat[:,0], 15.0*array(result), 15.0*array(result))
-    #ax.axis([-2,25,-0.2,2.0])
-    #plt.xlabel('Percentage of Time Spent Playing Video Games')
-    #plt.ylabel('Liters of Ice Cream Consumed Per Week')
+    ax.scatter(mat[:, 1], mat[:, 0], 15.0*array(result), 15.0*array(result))
+    # ax.axis([-2,25,-0.2,2.0])
+    # plt.xlabel('Percentage of Time Spent Playing Video Games')
+    # plt.ylabel('Liters of Ice Cream Consumed Per Week')
     plt.show()
 
-def autoNorm(dataSet):
-    minValues = dataSet.min(0)
-    maxValues = dataSet.max(0)
+
+def autoNorm(numSet):
+    minValues = numSet.min(0)
+    maxValues = numSet.max(0)
     ranges = maxValues - minValues
-    #normalDataSet = zeros(shape(dataSet))
-    m = dataSet.shape[0]
-    normalDataSet = dataSet - tile(minValues,(m,1))
-    normalDataSet = normalDataSet/tile(ranges,(m,1))
-    return normalDataSet , ranges , minValues
+    # normalDataSet = zeros(shape(dataSet))
+    m = numSet.shape[0] # shape指的是各个维度的大小
+    print(numSet.shape) # 返回一个各个维度的shape的列表
+    print(numSet.size) # 返回真-size
+    normalDataSet = numSet - tile(minValues, (m, 1))
+    normalDataSet = normalDataSet / tile(ranges, (m, 1))
+    return normalDataSet, ranges, minValues
 
-dataSet,ranges,minValues = autoNorm(returnMat)
 
+returnMat, classLabelVector = file2matrix("datingTestSet2.txt", "\t")
+dataSet, ranges1, minValues1 = autoNorm(returnMat)
 print(dataSet)
-print(ranges)
-print(minValues)
+print(ranges1)
+print(minValues1)
+mat2gra(dataSet, classLabelVector)
 
-def datingClass():
+
+def dating_class():
     hoRatio = 0.1
-    dataDataMat,datingLabels = file2matrix("datingTestSet2.txt","\t")
-    normal , ranges ,minValue =autoNorm(dataDataMat)
+    dataDataMat,datingLabels = file2matrix("datingTestSet2.txt", "\t")
+    normal, ranges, minValue = autoNorm(dataDataMat)
     m = normal.shape[0]
     numTestVec = int(m*hoRatio)
     errorCount = 0.0
     for i in range(numTestVec):
         break
-        #TODO:to be continue
+        # TODO:to be continue
 
-#mat2gra(*file2matrix("datingTestSet2.txt","\t"))
+
